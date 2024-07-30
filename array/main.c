27ba3ed02034c480ced6311f74b99eec499993a6
AmirzanJeyakumar <jeyakumaramirzan@gmail.com>
2024-07-30 19:12:23 +0100

    array: add linearSearch function

--- array/main.c
+++ array/main.c
@@ -49,11 +49,29 @@ int Delete(struct Array *arr, int index) {
     return 0;
 }
 
+void swap(int *x,int *y) {
+    int temp=*x;
+    *x=*y;
+    *y=temp;
+}
+
+int LinearSearch(struct Array *arr,int key) {
+    int i;
+
+    for(i=0;i<arr->length;i++) {
+        if(key==arr->A[i]) {
+            swap(&arr->A[i],&arr->A[0]);
+            return i;
+        }
+    }
+
+    return -1;
+}
 
 int main() { 
     struct Array arr ={{2,3,4,5,6},10,5};
 
-    printf("%d", Delete(&arr,4));
+    printf("%d", LinearSearch(&arr,4));
     Display(arr);
 
     return 0;
