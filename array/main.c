321ea7225f6d92048f47baf68cc1d2cec0a41960
AmirzanJeyakumar <jeyakumaramirzan@gmail.com>
2024-08-19 22:03:28 +0100

    array: add more functions

--- array/main.c
+++ array/main.c
@@ -68,10 +68,112 @@ int LinearSearch(struct Array *arr,int key) {
     return -1;
 }
 
-int main() { 
-    struct Array arr ={{2,3,4,5,6},10,5};
+int BinarySearch(struct Array arr, int key) {
+    int l, mid, h;
+    l=0;
+    h=arr.length-1;
 
-    printf("%d", LinearSearch(&arr,4));
+    while(l<=h) {
+        mid =(l+h)/2;
+        if (key==arr.A[mid]) {
+            return mid;
+        } else if (key<arr.A[mid]){
+            h=mid-1;
+        } else {
+            l=mid+1;
+        }
+    }
+    return -1;
+}
+
+int RBinSearch(int a[], int l, int h, int key) {
+    int mid;
+
+    if (l<=h) {
+        mid = (l+h)/2;
+        if (key==a[mid]) {
+            return mid;
+        } else if (key<a[mid]) {
+            return RBinSearch(a, l, mid-1, key);
+        } else {
+            return RBinSearch(a, mid+1, h, key);
+        }
+    }
+    return -1;
+}
+
+int Get(struct Array arr,int index) {
+    if (index>=0 && index<arr.length) {
+        return arr.A[index];
+    }
+    return -1;
+}
+
+void Set(struct Array *arr,int index,int x) {
+    if(index>=0 && index<arr->length) {
+        arr->A[index]=x;
+    }
+}
+
+int Max(struct Array arr) {
+    int max=arr.A[0];
+    int i;
+    for(i=1;i<arr.length;i++) {
+        if(arr.A[i]>max) {
+            max=arr.A[i];
+        }
+    }
+    return max;
+}
+
+int Min(struct Array arr) {
+    int min=arr.A[0];
+    int i;
+    for(i=1;i<arr.length;i++) {
+        if(arr.A[i]<min) {
+            min=arr.A[i];
+        }
+    }
+    return min;
+}
+
+int Sum(struct Array arr) {
+    int s=0;
+    int i;
+    for(i=0;i<arr.length;i++) {
+        s+=arr.A[i];
+    }
+    return s;
+}
+
+float Avg(struct Array arr) {
+    return (float)Sum(arr)/arr.length;
+}
+
+void Reverse(struct Array *arr) {
+    int *B;
+    int i,j;
+
+    B = (int *)malloc(arr->length*sizeof(int));
+    for (i=arr->length-1, j=0; i>=0; i--, j++) {
+        B[j] = arr->A[i];
+    }
+    for (i=0; i<arr->length;i++) {
+        arr->A[i] = B[i];
+    }
+}
+
+void Reverse2(struct Array *arr) {
+    int i,j;
+    for (i=0, j=arr->length-1; i<j; i++, j--) {
+        swap(&arr->A[i], &arr->A[j]);
+    }
+}
+
+int main() {
+    struct Array arr={{2,3,9,16,18,21,28,32,35},10,9};
+
+    Reverse2(&arr);
     Display(arr);
 
     return 0;
